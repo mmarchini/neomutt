0143038ae028c876255871e9caede9f5a102734d
Christopher John CZETTEL <chris@christopher-czettel.net>
2016-09-25 21:50:17 +0200

    icommand

--- icommands.c
+++ icommands.c
@@ -31,10 +31,19 @@
 #include "globals.h"
 #include "muttlib.h"
 #include "pager.h"
+#include "summary.h"
 #include "version.h"
 
 // clang-format off
+static enum CommandResult icmd_bind(struct     Buffer *, struct Buffer *, unsigned long, struct Buffer *);
+static enum CommandResult icmd_color(struct    Buffer *, struct Buffer *, unsigned long, struct Buffer *);
+static enum CommandResult icmd_help(struct     Buffer *, struct Buffer *, unsigned long, struct Buffer *);
+static enum CommandResult icmd_messages(struct Buffer *, struct Buffer *, unsigned long, struct Buffer *);
+static enum CommandResult icmd_quit(struct     Buffer *, struct Buffer *, unsigned long, struct Buffer *);
+static enum CommandResult icmd_scripts(struct  Buffer *, struct Buffer *, unsigned long, struct Buffer *);
 static enum CommandResult icmd_set(struct      Buffer *, struct Buffer *, unsigned long, struct Buffer *);
+static enum CommandResult icmd_test(struct     Buffer *, struct Buffer *, unsigned long, struct Buffer *);
+static enum CommandResult icmd_vars(struct     Buffer *, struct Buffer *, unsigned long, struct Buffer *);
 static enum CommandResult icmd_version(struct  Buffer *, struct Buffer *, unsigned long, struct Buffer *);
 
 /**
@@ -43,7 +52,19 @@ static enum CommandResult icmd_version(struct  Buffer *, struct Buffer *, unsign
  * @note These commands take precendence over conventional mutt rc-lines
  */
 const struct ICommand ICommandList[] = {
+  { "bind",     icmd_bind,     0 },
+  { "color",    icmd_color,    0 },
+  { "help",     icmd_help,     0 },
+  { "macro",    icmd_bind,     0 },
+  { "messages", icmd_messages, 0 },
+  { "q!",       icmd_quit,     0 },
+  { "q",        icmd_quit,     0 },
+  { "qa",       icmd_quit,     0 },
+  { "quit",     icmd_quit,     0 },
+  { "scripts",  icmd_scripts,  0 },
   { "set",      icmd_set,      0 },
+  { "test",     icmd_test,     0 },
+  { "vars",     icmd_vars,     0 },
   { "version",  icmd_version,  0 },
   { NULL,       NULL,          0 },
 };
@@ -78,6 +99,7 @@ enum CommandResult mutt_parse_icommand(/* const */ char *line, struct Buffer *er
   SKIPWS(expn.dptr);
   while (*expn.dptr)
   {
+    /* TODO: contemplate implementing a icommand specific tokenizer */
     mutt_extract_token(&token, &expn, 0);
     for (size_t i = 0; ICommandList[i].name; i++)
     {
@@ -96,6 +118,74 @@ finish:
   if (expn.destroy)
     FREE(&expn.data);
   return rc;
+}
+
+/*
+ *  wrapper functions to prepare and call other functionality within mutt
+ *  see icmd_quit and icmd_help for easy examples
+ */
+static enum CommandResult icmd_quit(struct Buffer *buf, struct Buffer *s,
+                                    unsigned long data, struct Buffer *err)
+{
+  /* TODO: exit more gracefully */
+  mutt_exit(0);
+  return MUTT_CMD_SUCCESS;
+}
+
+static enum CommandResult icmd_help(struct Buffer *buf, struct Buffer *s,
+                                    unsigned long data, struct Buffer *err)
+{
+  /* TODO: implement ':help' command as suggested by flatcap in #162 */
+  mutt_buffer_addstr(err, _("Not implemented yet."));
+  return MUTT_CMD_ERROR;
+}
+
+static enum CommandResult icmd_test(struct Buffer *buf, struct Buffer *s,
+                                    unsigned long data, struct Buffer *err)
+{
+  mutt_summary();
+  return MUTT_CMD_SUCCESS;
+}
+
+static enum CommandResult icmd_bind(struct Buffer *buf, struct Buffer *s,
+                                    unsigned long data, struct Buffer *err)
+{
+  /* TODO: implement ':bind' and ':macro' command as suggested by flatcap in #162 */
+  mutt_buffer_addstr(err, _("Not implemented yet."));
+  return MUTT_CMD_ERROR;
+}
+
+static enum CommandResult icmd_color(struct Buffer *buf, struct Buffer *s,
+                                     unsigned long data, struct Buffer *err)
+
+{
+  /* TODO: implement ':color' command as suggested by flatcap in #162 */
+  mutt_buffer_addstr(err, _("Not implemented yet."));
+  return MUTT_CMD_ERROR;
+}
+
+static enum CommandResult icmd_messages(struct Buffer *buf, struct Buffer *s,
+                                        unsigned long data, struct Buffer *err)
+{
+  /* TODO: implement ':messages' command as suggested by flatcap in #162 */
+  mutt_buffer_addstr(err, _("Not implemented yet."));
+  return MUTT_CMD_ERROR;
+}
+
+static enum CommandResult icmd_scripts(struct Buffer *buf, struct Buffer *s,
+                                       unsigned long data, struct Buffer *err)
+{
+  /* TODO: implement ':scripts' command as suggested by flatcap in #162 */
+  mutt_buffer_addstr(err, _("Not implemented yet."));
+  return MUTT_CMD_ERROR;
+}
+
+static enum CommandResult icmd_vars(struct Buffer *buf, struct Buffer *s,
+                                    unsigned long data, struct Buffer *err)
+{
+  /* TODO: implement ':vars' command as suggested by flatcap in #162 */
+  mutt_buffer_addstr(err, _("Not implemented yet."));
+  return MUTT_CMD_ERROR;
 }
 
 /**
@@ -142,7 +232,7 @@ static enum CommandResult icmd_set(struct Buffer *buf, struct Buffer *s,
 }
 
 /**
- * icmd_set - Parse 'version' command - Implements ::icommand_t
+ * icmd_version - Parse 'version' command - Implements ::icommand_t
  */
 static enum CommandResult icmd_version(struct Buffer *buf, struct Buffer *s,
                                        unsigned long data, struct Buffer *err)
